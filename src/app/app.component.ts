3344bf0d449ebb333da1c06264312277349137c5
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2020-03-16 15:41:10 +0100

    uso estrapolazioni e proprerty binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoBergamo1';
+
+  htmlString = '<div><p>prova</p></div>'
+
+  getVal(){
+    return 2;
+  }
 }
